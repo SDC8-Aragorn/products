8dab02dd7a608adf4bfc8a11231cf981a732fb7a
Michael Fornwalt <mdfornwalt@gmail.com>
2021-11-18 15:34:08 -0700

    /products/:product_id endpoint now properly performs two queries to append features onto product object and return to server in legacy API data shape

--- database/index.js
+++ database/index.js
@@ -3,9 +3,42 @@ var connectionString = "postgresql://localhost/products";
 var pgClient = new pg.Client(connectionString);
 pgClient.connect();
 
-// var query = pgClient.query("SELECT id from products where id = '1'");
+const getProducts = async () => {
+  return pgClient
+    .query("SELECT * from product LIMIT 10")
+    .then(res => res.rows)
+    .catch(e => console.error(e.stack))
+}
 
-pgClient
-  .query("SELECT * from product where id = '1'")
-  .then(res => console.log(res.rows))
+const getProduct = async (id) => {
+
+  let queryOne = await pgClient.query(`SELECT * from product WHERE id = ${id}`)
+  let queryTwo = await pgClient.query(`SELECT feature, value FROM features WHERE product_id = ${id}`)
+
+  let resultOne = await queryOne.rows
+  let resultTwo = await queryTwo.rows
+
+  return [resultOne, resultTwo]
+}
+
+
+// const getProduct = async (id) => {
+
+//   return pgClient
+//     .query(`SELECT * from product WHERE id = ${id}`)
+//     .then(res => res.rows)
+//     .catch(e => console.error(e.stack))
+// }
+
+
+const getStyles = async (id) => {
+  return pgClient
+  .query(`SELECT * from product WHERE id = ${id}`)
+  .then(res => res.rows)
   .catch(e => console.error(e.stack))
+}
+
+module.exports = {
+  getProducts,
+  getProduct
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const app = express()
+const db = require('../database/index.js')
 
 const PORT = 3000
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('Please specify an endpoint!')
 })
+
+app.get('/products', function (req, res) {
+  db.getProducts()
+  .then(result => {
+    res.json(result)
+  })
+  .catch(err => {
+    res.send('Error retrieving products!')
+  })
+})
+
+app.get('/products/:product_id', function (req, res) {
+  db.getProduct(req.params.product_id)
+  .then (result => {
+    let product = result[0][0]
+    product.features = result[1]
+    res.json(product)
+  })
+})
+
+//STYLES
+// app.get('/products/:product_id/styles', function (req, res) {
+//   db.getStyles(req.params.product_id)
+//   .then (result => {
+//     res.json(result)
+//   })
+// })
+
 
 app.listen(PORT, ()=> {
   console.log(`Server listening on port: ${PORT}`)
