e66199340d0dd577a6d303485e5063a74c353384
Michael Fornwalt <mdfornwalt@gmail.com>
2021-11-20 13:50:13 -0700

    Product unit tests complete

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,16 +1,14 @@
 const db = require('../database/index.js')
 
-jest.setTimeout(20000)
-
-test('getProduct exists', () => {
+test('1. getProduct exists', () => {
   expect(db.getProduct).toBeTruthy()
 })
 
-test('getProduct is a function', () => {
+test('2. getProduct is a function', () => {
   expect(typeof db.getProduct).toBe('function')
 })
 
-test('getProduct returns an array', done => {
+test('3. getProduct returns an array', done => {
   db.getProduct(50)
   .then(result => {
     expect(Array.isArray(result)).toBe(true)
@@ -18,7 +16,7 @@ test('getProduct returns an array', done => {
   })
 });
 
-test('getProduct returned array contains two values', done => {
+test('4. getProduct returned array contains two values', done => {
   db.getProduct(50)
   .then(result => {
     expect(result.length).toBe(2)
@@ -26,7 +24,7 @@ test('getProduct returned array contains two values', done => {
   })
 });
 
-test('getProduct first value has 6 properties', done => {
+test('5 .getProduct first value has 6 properties', done => {
   db.getProduct(20)
   .then(result => {
     let props = Object.keys(result[0][0])
@@ -34,6 +32,78 @@ test('getProduct first value has 6 properties', done => {
     done()
   })
 })
+
+test('1 .getProducts exists', () => {
+  expect(db.getProducts).toBeTruthy()
+})
+
+test('2 .getProducts is a function', () => {
+  expect(typeof db.getProducts).toBe('function')
+})
+
+test('3. getProducts returns an object', done => {
+  db.getProducts(50)
+  .then(result => {
+    expect(Array.isArray(result)).toBe(true)
+    done()
+  })
+});
+
+test('4. getProducts returns 5 values', done => {
+  db.getProducts(50)
+  .then(result => {
+    expect(result.length).toBe(5)
+    done()
+  })
+});
+
+test('1. getStyles exists', () => {
+  expect(db.getStyles).toBeTruthy()
+})
+
+test('2. getStyles is a function', () => {
+  expect(typeof db.getStyles).toBe('function')
+})
+
+test('3. getStyles returns an object', done => {
+  db.getStyles(50)
+  .then(result => {
+    expect(typeof result).toBe('object')
+    done()
+  })
+});
+
+test('4. getStyles return has a product_id property', done => {
+  db.getStyles(50)
+  .then(result => {
+    expect(result.product_id).toBeTruthy()
+    done()
+  })
+})
+
+test('5. getStyles return has a results property', done => {
+  db.getStyles(50)
+  .then(result => {
+    expect(result.results).toBeTruthy()
+    done()
+  })
+})
+
+test('1 .getRelated exists', () => {
+  expect(db.getRelated).toBeTruthy()
+})
+
+test('2 .getRelated is a function', () => {
+  expect(typeof db.getRelated).toBe('function')
+})
+
+test('3. getRelated returns an array', done => {
+  db.getRelated(50)
+  .then(result => {
+    expect(Array.isArray(result)).toBe(true)
+    done()
+  })
+});
 
 afterAll(() => {
   return db.close()
